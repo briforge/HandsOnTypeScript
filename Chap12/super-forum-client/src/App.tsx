3b4ecfe40fb77aee8b3728aafda635965d93f524
briforge <briforge@gmail.com>
2022-01-05 21:28:25 -0500

    add navigation stuff

--- Chap12/super-forum-client/src/App.tsx
+++ Chap12/super-forum-client/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main';
+import Nav from './components/Nav';
 import LeftMenu from './components/LeftMenu';
 
 function App() {
   return (
     <div className="App">
-      <nav className="navigation">Nav</nav>
+      <nav className="navigation">
+        <Nav />
+      </nav>
       <div className="sidebar">Sidebar</div>
       <LeftMenu/>
       <Main/>
---
+++ Chap12/super-forum-client/src/components/sidebar/SideBar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useWindowDimensions } from "../../hooks/useWindowDimensions";
+import SideBarMenus from "./SideBarMenus";
+
+const Sidebar = () => {
+    const { width } = useWindowDimensions();
+    if (width <= 768) {
+        return null;
+    }
+    return (
+        <div className="sidebar">
+            <SideBarMenus />
+        </div>
+    );
+};
+
+export default Sidebar;
